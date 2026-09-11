a2937537b70f4b4270bb3e95dd9983c3b0f57a15
deepaksingh63584 <thakursingh63584@gmail.com>
2026-09-12 00:48:03 +0530

    first repeat non char

---
+++ StringProblems/src/FirstNonRepeatCharInString.java
@@ -0,0 +1,21 @@
+public class FirstNonRepeatCharInString {
+    public static void main(String[] args) {
+        String str = "aabbccde";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (ch == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println(ch);
+                break;
+            }
+        }
+    }
+}

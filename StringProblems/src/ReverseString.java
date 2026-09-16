408a554330903d1ca0ea3ad90a6b236102565d05
deepaksingh63584 <thakursingh63584@gmail.com>
2026-09-16 22:16:23 +0530

    Reverse string

---
+++ StringProblems/src/ReverseString.java
@@ -0,0 +1,20 @@
+public class ReverseString {
+    public static String reverse(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("Hello Deepak"));
+    }
+}
